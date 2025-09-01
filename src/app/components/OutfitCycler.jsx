9756ac2de019bd07a8d26d473da5f609bfa6bd68
Luc Nguyen <lucnguyen1416@gmail.com>
2025-08-31 21:26:36 -0700

    added cycle functionality, added outerwear

--- src/app/components/OutfitCycler.jsx
+++ src/app/components/OutfitCycler.jsx
@@ -4,22 +4,35 @@
 import { useState } from "react";
 
 export default function OutfitCycler() {
-  const tops = ["/images/tops/T1.png", "/images/tops/T2.png", "/images/tops/T3.png"];
-  const bottoms = ["/images/bottoms/B1.png", "/images/bottoms/B2.png"];
-  const shoes = ["/images/shoes/S1.png", "/images/shoes/S2.png"];
+    const tops = ["/images/tops/T1.png", "/images/tops/T2.png", "/images/tops/T3.png"];
+    const bottoms = ["/images/bottoms/B1.png", "/images/bottoms/B2.png", "/images/bottoms/B3.png", 
+    "/images/bottoms/B4.png", "/images/bottoms/B5.png"];
+    const shoes = ["/images/shoes/S1.png", "/images/shoes/S2.png", "/images/shoes/S3.png"];
 
-  const [topIndex, setTopIndex] = useState(0);
-  const [bottomIndex, setBottomIndex] = useState(0);
-  const [shoesIndex, setShoesIndex] = useState(0);
+    const outerwear = ["/images/outer/O1.png", "/images/outer/O2.png", "/images/outer/O3.png"];
 
-  const nextTop = () => setTopIndex((topIndex + 1) % tops.length);
-  const nextBottoms = () => setBottomIndex((bottomIndex + 1) % bottoms.length);
-  const nextShoes = () => setShoesIndex((shoesIndex + 1) % shoes.length);
+    const [topIndex, setTopIndex] = useState(0);
+    const [bottomIndex, setBottomIndex] = useState(0);
+    const [shoesIndex, setShoesIndex] = useState(0);
+    const [outerIndex, setOuterIndex] = useState(0);
 
+  const [selectedCategory, setSelectedCategory] = useState(null); // "top", "bottom", "shoes"
 
-  return (
+  const next = () => {
+    if (selectedCategory === "top") setTopIndex((i) => (i + 1) % tops.length);
+    else if (selectedCategory === "bottom") setBottomIndex((i) => (i + 1) % bottoms.length);
+    else if (selectedCategory === "shoes") setShoesIndex((i) => (i + 1) % shoes.length);
+    else if (selectedCategory === "outer") setOuterIndex((i) => (i + 1) % outerwear.length);
+  };
 
-    
+  const back = () => {
+    if (selectedCategory === "top") setTopIndex((i) => (i - 1 + tops.length) % tops.length);
+    else if (selectedCategory === "bottom") setBottomIndex((i) => (i - 1 + bottoms.length) % bottoms.length);
+    else if (selectedCategory === "shoes") setShoesIndex((i) => (i - 1 + shoes.length) % shoes.length);
+    else if (selectedCategory === "outer") setOuterIndex((i) => (i - 1 + outerwear.length) % outerwear.length);
+  };
+
+  return (    
     <div style={{ 
         // display: "flex", 
         // flexDirection: "column", 
@@ -31,44 +44,54 @@ export default function OutfitCycler() {
       }} className="flex items-center justify-center h-screen w-screen">
 
         <div className="flex items-center justify-center"> 
-            <div className="w-64 h-[650px] bg-gray-300 border-2 border-gray-400 rounded-lg flex flex-col items-center justify-between">
+            <div className="w-64 h-[650px] bg-white border-2 border-gray-400 rounded-lg flex flex-col items-center justify-between">
 
-                <div className="flex-1 flex items-center justify-center border-b border-gray-600 h-[200px] w-full">
+                <div className={`flex-1 flex items-center justify-center border-gray-600 h-[200px] w-full cursor-pointer
+                ${selectedCategory === "top" ? "border-gray-600 border-4" : ""}`}
+                onClick={() => setSelectedCategory("top")}
+                >
                     <img src={tops[topIndex]} alt="Top" className="block h-full w-auto object-contain " />
                 </div>
 
 
-                <div className="flex-1 flex items-center justify-center border-b border-gray-600 h-[300px] w-full">
+                <div className={`flex-1 flex items-center justify-center border-gray-600 h-[300px] w-full cursor-pointer
+                                ${selectedCategory === "bottom" ? "border-gray-600 border-4" : ""}`}
+                                onClick={() => setSelectedCategory("bottom")}
+                                >
+                
                     <img src={bottoms[bottomIndex]} alt="Bottoms" className="block h-full w-auto object-contain" />
                 </div>
 
 
-                <div className="flex-1 flex items-center justify-center h-[150px] w-full">
-                    <img src={shoes[shoesIndex]} alt="Shoes" className="block h-full w-auto object-contain" />
-                </div>
+                <div className={`flex-1 flex items-center justify-center border-gray-600 h-[150px] w-full cursor-pointer
+                                ${selectedCategory === "shoes" ? "border-gray-600 border-4" : ""}`}
+                                onClick={() => setSelectedCategory("shoes")}
+                                >
 
+                    <img src={shoes[shoesIndex]} alt="Shoes" className="block h-full w-auto object-contain pointer-events-none"/>
+                </div>
 
             </div>
         </div>
 
-      {/* Controls */}
-      {/* <div style={{ display: "flex", gap: "10px" }}>
-        <button onClick={nextTop}>Next Top</button>
-        <button onClick={nextBottoms}>Next Pants</button>
-        <button onClick={nextShoes}>Next Shoes</button>
-
-        <button className="px-4 py-2 border border-blue-500 text-blue-500 rounded-lg hover:bg-blue-50">
-            Next Outfit
-        </button>
-
-      </div> */}
+          {/* Outerwear box positioned to the right */}
+        <div className={`absolute left-[65%] top-50 w-52 h-[300px] bg-white border-2 border-gray-400 rounded-lg flex items-center justify-center cursor-pointer
+                                ${selectedCategory === "outer" ? "border-gray-600 border-4" : ""}`}
+                                onClick={() => setSelectedCategory("outer")}
+                                >
+            <img src={outerwear[outerIndex]} alt="Outerwear" className="h-full w-auto object-contain" />
+        </div>
 
         {/* Fixed Bottom Buttons */}
         <div className="fixed bottom-10 left-0 right-0 flex justify-between px-10">
-            <button className="px-16 py-8 bg-gray-500 text-white rounded-lg hover:bg-gray-600 border-4 border-gray-600 cursor-pointer">
+            <button className="px-16 py-8 bg-gray-500 text-white rounded-lg hover:bg-gray-600 border-4 border-gray-600 cursor-pointer"
+            onClick={back}
+            >
             Back
             </button>
-            <button className="px-16 py-8 bg-blue-500 text-white rounded-lg hover:bg-blue-600 border-4 border-gray-600 cursor-pointer">
+            <button className="px-16 py-8 bg-blue-500 text-white rounded-lg hover:bg-blue-600 border-4 border-gray-600 cursor-pointer"
+            onClick={next}
+            >
             Next
             </button>
         </div>
